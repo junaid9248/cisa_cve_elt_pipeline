20783d3f1e65cb9993979a28599205d2bf3c8cf8
Mohammed Junaid <junaidwork456@gmail.com>
2025-12-26 19:49:21 -0600

    Updated table tests script to pass queries in a list and more

--- tests/table_test.py
+++ tests/table_test.py
@@ -13,21 +13,40 @@ def test_table():
     try:
         dataset_id = f'{googleclient.projectID}.cve_all'
         table_id = dataset_id + '.cve_combined_final_table'
-        
-        test_query = f'''
+
+        queries_list = [
+
+            {'query1':
+                f'''
+                SELECT * from `{table_id}`
+                WHERE cve_id IS NULL or cve_id = '' 
+                    '''},
+            {'query2':
+             f'''
             SELECT cve_id FROM `{table_id}`
-            WHERE regexp_contains(cve_id, r"^CVE-1999-") OR REGEXP_CONTAINS(cve_id, r"^CVE-2000-")
-        '''
+            WHERE regexp_contains(cve_id, r"^CVE-1999-") OR REGEXP_CONTAINS(cve_id, r"^CVE-2000-")'''},
+        ]
 
-        query_job = bigquery_client.query(test_query)
-        # The returned object contains errors if the job fails.
-        results = query_job.result()
-        result_df = results.to_dataframe()
+        for query in queries_list:
+            querynum = list(query.keys())[0]
 
-        if result_df.empty:
-            raise RuntimeError("DQ failed: no CVE IDs from 1999 or 2000 found.")
-        else:
-            logging.info("Data quality check PASSED: found CVE IDs from 1999 or 2000.")
+            query_job = bigquery_client.query(query=query.get(querynum))
+            # The returned object contains errors if the job fails.
+            results = query_job.result()
+            result_df = results.to_dataframe()
+
+            match querynum:
+                case 'query1':
+                    if not result_df.empty:
+                        raise RuntimeError("DQ check FAILED: SOME CVE values are missing")
+                    else:
+                        logging.info("DQ check PASSED: All CVE entries have values.")
+
+                case 'query2':
+                    if result_df.empty:
+                        raise RuntimeError('DQ check FAILED: There are entries missing from 1999 and 2000')
+                    else:
+                        logging.info('DQ check PASSED: CVE entries contain entries from 1999 and 2000 ')
 
     except Exception as e:
         logging.error(f"Failed to access BigQuery table: {e}")
